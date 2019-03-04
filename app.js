3d0f66bbdca73e880fceeb94f2ac117ed0faf061
Remy Oleszczuk <remy.o@outlook.com>
2019-03-04 10:58:17 +0100

    with client

--- app.js
+++ app.js
@@ -15,20 +15,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
 app.use('/', checkoutRouter);
 app.use('/', notification);
 
- 
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 // catch 404 and forward to error handler 
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -38,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
+  res.render('error'); 
+}); 
 
 module.exports = app;
