439e0f776030181faa2cfeda83ba3762b7ec7e64
Remy Oleszczuk <remy.o@outlook.com>
2019-03-04 14:02:37 +0100

    mobile v2

--- client/src/components/Home/Slider.js
+++ client/src/components/Home/Slider.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
 let kawa = require("../../assets/img/slider/kawa.png");
-
+let zl = require("../../assets/img/slider/zl.png");
 require("./Slider.scss");
 
 export default class SliderS extends Component {
@@ -20,6 +20,33 @@ export default class SliderS extends Component {
     return (
       <div className="slider-home">
         <Slider {...settings}>
+        <div className="border">
+            <div className="border-line-v-1" />
+            <div className="border-line-v-2" />
+            <div className="border-line-h-1" />
+            <div className="border-line-h-2" />
+            <div className="border-top" />
+            <div className="border-bottom" />{" "}
+            <div className="slider-content">
+              <div className="slider-image">
+                <img src={zl} />
+              </div>
+
+              <div className="slider-copy">
+                <div className="header-text">pomagaj razem z kawerką</div>
+                <div className="copy">
+                  <div className="arrow" />
+                  <p>
+                  Za każde sprzedane 250g kawy w ofercie detalicznej, przeznaczamy 1zł na rzecz Fundacji Siepomaga.
+                  </p>
+                  <a href="/pomagaj-z-kawerka" className="button button-red">
+                    ZOBACZ WIĘCEJ!
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          </div>
           <div className="border">
             <div className="border-line-v-1" />
             <div className="border-line-v-2" />
@@ -44,9 +71,6 @@ export default class SliderS extends Component {
                     Wszystkie ziarna które używamy są etycznie pozyskiwane oraz
                     pochodzą z upraw FAIR TRADE.
                   </p>
-                  <p>
-                    `
-                  </p>
                   <a href="/" className="button button-red">
                     ZOBACZ WIĘCEJ!
                   </a>
@@ -54,9 +78,7 @@ export default class SliderS extends Component {
               </div>
             </div>
           </div>
-          <div>
-            <h3>2</h3>
-          </div>
+        
         </Slider>
       </div>
     );
